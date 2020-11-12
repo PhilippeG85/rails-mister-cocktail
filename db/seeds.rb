801f17a92d603529f68691b311524ae8aa50683d
Philippe Gonse <pgonse@gmail.com>
2020-11-12 16:53:37 -0500

    mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Marry")
+Cocktail.create(name: "Pastis")
+
+Dose.create(description: "8oz", ingredient_id: 1, cocktail_id: 1)
+Dose.create(description: "8oz", ingredient_id: 2, cocktail_id: 2)
+Dose.create(description: "8oz", ingredient_id: 3, cocktail_id: 3)
